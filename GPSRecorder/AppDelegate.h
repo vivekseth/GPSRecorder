54e226a00a2dfe508f85d4e42df63a63b0851d1f
Vivek Seth <Manu@Deepak-Seths-MacBook.local>
2012-06-30 11:57:25 -0400

    Initial Commit

---
+++ GPSRecorder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GPSRecorder
+//
+//  Created by Vivek Seth on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GPSRecorder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GPSRecorder
+//
+//  Created by Vivek Seth on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
